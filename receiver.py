4285fd4afd0302674e0c3f9cec0b53b1ed206a68
smk333 <43208864+smk333@users.noreply.github.com>
2020-04-16 19:44:44 -0400

    Update receiver.py
    Added key rebinding (needs testing)

--- receiver.py
+++ receiver.py
@@ -1,9 +1,18 @@
 import paho.mqtt.client as mqtt
-from pynput.keyboard import Key, Controller
+from pynput.keyboard import Key, Controller, Listener
 
 MQTT_SERVER = "localhost"
 MQTT_PATH = "test_channel"
 keyboard = Controller()
+
+leftKey = Key.left
+rightKey = Key.right
+forwardKey = Key.media_volume_up
+backwardsKey = Key.media_volume_down
+
+def on_press(key):
+    return(key)
+
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -18,23 +27,37 @@ def on_message(client, userdata, msg):
     #msg.topic+" "+
     print(str(msg.payload))
     if str(msg.payload) == "b'Left'":
-        keyboard.press(Key.left)
-        keyboard.release(Key.left)
+        keyboard.press(leftKey)
+        keyboard.release(leftKey)
     
     if str(msg.payload) == "b'Right'":
-        keyboard.press(Key.right)
-        keyboard.release(Key.right)
+        keyboard.press(rightKey)
+        keyboard.release(rightKey)
     
     if str(msg.payload) == "b'Backwards'":
-        keyboard.press(Key.media_volume_up)
-        keyboard.release(Key.media_volume_up)
+        keyboard.press(backwardsKey)
+        keyboard.release(backwardsKey)
 
     if str(msg.payload) == "b'Forward'":
-        keyboard.press(Key.media_volume_down)
-        keyboard.release(Key.media_volume_down)
+        keyboard.press(forwardKey)
+        keyboard.release(forwardKey)
         #keyboard.release(Key.menu)
         #keyboard.release(Key.up)
     # more callbacks, etc
+    
+answer = input("Do you wish to change the default key bindings? y/n :")
+if answer = 'y':
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
+        print("Input the key to press when moving the controller left.\n")
+        leftKey = on_press(key)
+        print("Input the key to press when moving the controller right.\n")
+        rightKey = on_press(key)
+        print("Input the key to press when moving the controller forward.\n")
+        forwardKey = on_press(key)
+        print("Input the key to press when moving the controller back.\n")
+        backwardsKey = on_press(key)
+        listener.stop()
  
 client = mqtt.Client()
 client.on_connect = on_connect
